8d9dc55e25aae3584d09e3ab74707f8dd27c1e7d
Josue-RS <josue.ruiz@wizeline.com>
2023-12-27 20:56:26 -0600

    FIX: mvn project build

---
+++ src/main/java/com/vioc/pos/store/viocposstorevehicle/ViocposStoreVehicleApplication.java
@@ -0,0 +1,13 @@
+package com.vioc.pos.store.viocposstorevehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViocposStoreVehicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViocposStoreVehicleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vioc/pos/store/viocposstorevehicle/ViocposStoreVehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vioc.pos.store.viocposstorevehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViocposStoreVehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
